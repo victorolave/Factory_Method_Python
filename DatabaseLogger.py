b8eec316c80570d29681625bb7cd28478ec01a8d
Victor Olave <victorolave1131@gmail.com>
2019-08-08 23:36:09 -0500

    Database Logger

--- DatabaseLogger.py
+++ DatabaseLogger.py
@@ -1,6 +1,21 @@
+import mysql.connector
 from logger import Logger
 
+dbConnect = {
+    'host':'localhost',
+    'user':'root',
+    'password':'victor1131',
+    'database':'messages'
+}
+
+conexion = mysql.connector.connect(**dbConnect)
 
 class DatabaseLogger(Logger):
     def __init__(self, msg):
+        cursor = conexion.cursor()
+        sql = "INSERT INTO messages (id, message) VALUES (%s,%s)"
+        cursor.execute(sql, (None, msg))
+        conexion.commit()
+
+        print("Message: " + msg + ", saved in database")
         print("Database Message:  " + msg)
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import LoggerFactory
 
 if __name__ == '__main__':
     logger_factory = LoggerFactory.LoggerFactory()
-    logger_factory.get_log("Hello World", "File")
+    logger_factory.get_log("Hello Python", "Database")
